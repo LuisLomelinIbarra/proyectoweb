d1466cbc87abf59b96663cf5b9e9c538cdf10e3e
Luis F. Lomelín Ibarra <luis.f.lomelin.il@gmail.com>
2021-11-20 19:22:29 -0600

    Se agrego el controlador de forgot pass y se agrego el css a lo de login

---
+++ controllers/change_pass.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    include($_SERVER['DOCUMENT_ROOT']."/proyectoweb/models/dbconn.php");
+    include($_SERVER['DOCUMENT_ROOT']."/proyectoweb/controllers/functions.php");
+    include ($_SERVER['DOCUMENT_ROOT']."/proyectoweb/views/forgotpass.html");
